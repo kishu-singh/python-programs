320164b08d400984fc441e08c76ca244992b0f28
kishu <kishu450kk@gmail.com>
2022-08-22 22:42:49 +0530

    added new file

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+print("For addition inter here your input and get the result /n thank you!")
 a = int(input("Enter the first number : "))
 b = int(input("Enter the second number : "))
-print(a+b)
+print(a + b)
+
+print("For subtraction inter here your input and get the result /n thank you!")
+a = int(input("Enter the first number : "))
+b = int(input("Enter the second number : "))
+print(a - b)
