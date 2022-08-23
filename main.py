6a9dbe920fb62bf77f20c445994cb726710742dd
kishu <kishu450kk@gmail.com>
2022-08-23 23:56:32 +0530

    Odd & Even number

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-print("For addition inter here your input and get the result /n thank you!")
-a = int(input("Enter the first number : "))
-b = int(input("Enter the second number : "))
-print(a + b)
-
-print("For subtraction inter here your input and get the result /n thank you!")
-a = int(input("Enter the first number : "))
-b = int(input("Enter the second number : "))
-print(a - b)
+# print("For addition inter here your input and get the result /n thank you!")
+# a = int(input("Enter the first number : "))
+# b = int(input("Enter the second number : "))
+# print(a + b)
+#
+# print("For subtraction inter here your input and get the result /n thank you!")
+# a = int(input("Enter the first number : "))
+# b = int(input("Enter the second number : "))
+# print(a - b)
+a = 6
+b = 7
+if (a % 2 == 0):
+    print("even")
+else:
+    print("odd")
