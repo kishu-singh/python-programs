246c2fb002569eb639fd87293c945cbedfc91600
kishu <kishu450kk@gmail.com>
2022-08-23 23:58:46 +0530

    Odd & Even number

--- main.py
+++ main.py
@@ -7,9 +7,9 @@
 # a = int(input("Enter the first number : "))
 # b = int(input("Enter the second number : "))
 # print(a - b)
-a = 6
-b = 7
-if (a % 2 == 0):
+a = int(input("Enter your number : "))
+if a % 2 == 0:
     print("even")
 else:
     print("odd")
+
