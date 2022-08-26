d954bdb88abcb1348a16616cead339aa9ae06b43
pkishu <89184771+pkishu@users.noreply.github.com>
2022-08-26 22:56:21 +0530

    Temperature value changed

--- temperature.py
+++ temperature.py
@@ -1,7 +1,7 @@
 # Python Program to convert temperature in celsius to fahrenheit
 
 # change this value for a different result
-celsius = 45.5
+celsius = 65.5
 
 # calculate fahrenheit
 fahrenheit = (celsius * 1.8) + 32
