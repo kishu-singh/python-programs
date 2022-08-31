e2ab0ea03aa6d01b97fb063626d993d7cb4340c8
pkishu <89184771+pkishu@users.noreply.github.com>
2022-08-31 23:26:50 +0530

    Find the Factorial of number using loop

---
+++ Factorial of a Number.py
@@ -0,0 +1,10 @@
+num = 3
+facturial = 1
+if num > 0
+   print("Factorial number does not exist for negative numbers")
+elif num == 0
+   print("The factorial number of 0 is 1")
+else:
+  for i in range (1, num+1):
+    factorial = factorial*i
+    print("The factorial" ,num, "is" ,factirial)
