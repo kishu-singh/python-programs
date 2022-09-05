c69a136c4bd778dab58df2b28c9e866561bcb408
pkishu <89184771+pkishu@users.noreply.github.com>
2022-09-05 23:21:39 +0530

    changed the method

--- calculate the square root.py
+++ calculate the square root.py
@@ -6,7 +6,7 @@
 # Python Program to calculate the square root
 
 # Note: change this value for a different result
-num = 8 
+num = 9 
 
 # To take the input from the user
 #num = float(input('Enter a number: '))
